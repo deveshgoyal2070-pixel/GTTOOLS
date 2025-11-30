ae9c31d88921b257bd1d30dbacc10e278dc04d09
deveshgoyal2070-pixel <deveshgoyal.2070@gmail.com>
2025-11-30 10:22:25 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+const welcomeBox = document.getElementById("welcomeBox");
+
+function selectTool(id) {
+  if (welcomeBox) welcomeBox.classList.add("hidden");
+}
+
+function applyFilter() {}
+function resetImageFilters() {}
+function downloadImage() {}
